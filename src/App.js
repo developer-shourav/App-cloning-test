cef7b2f863cd93511b36680121985b6c78df39a7
Mrinmoy Raj <Mrinmoyraj823@gmail.com>
2021-10-03 10:46:11 +0600

    re push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
 import Shop from './components/Shop/Shop';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Header></Header>
       <Shop></Shop>
+      <Inventory></Inventory>
     </div>
   );
 }
---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h1>This is inventory</h1>
+        </div>
+    );
+};
+
+export default Inventory;
